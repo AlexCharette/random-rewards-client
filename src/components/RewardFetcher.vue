26d58739f9dd33e9a2b3767a35294188eda8921c
Alexander Charette <ga_charette@pm.me>
2022-10-25 21:13:53 +0200

    Modularize alert content for rewards

--- src/components/RewardFetcher.vue
+++ src/components/RewardFetcher.vue
@@ -1,33 +1,70 @@
 <template>
   <div id="container">
+    <ion-alert
+      :is-open="isOpenRef"
+      :header="alertContent.header"
+      :sub-header="alertContent.subHeader"
+      :message="alertContent.message"
+      :buttons="alertContent.buttons"
+      @didDismiss="setOpen(false)"
+    ></ion-alert>
     <ion-button @click="fetchReward" shape="round">Time for a reward!</ion-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { IonButton } from '@ionic/vue'
+import { defineComponent, ref } from 'vue'
+import { IonAlert, IonButton } from '@ionic/vue'
 import { mapState } from 'vuex'
 
 export default defineComponent({
   name: 'RewardFetcher',
-  components: { IonButton },
+  components: { IonAlert, IonButton },
   computed: {
     ...mapState('rewards', ['rewards'])
+  },
+  data() {
+    return {
+      alertContent: {
+        header: '',
+        subHeader: '',
+        message: '',
+        buttons: ['OK!']
+      }
+    }
+  },
+  setup() {
+    const isOpenRef = ref(false);
+    const setOpen = (state: boolean) => isOpenRef.value = state;
+    
+    return { isOpenRef, setOpen }
   },
   methods: {
     fetchReward() {
       const randomChance = Math.floor(Math.random() * 3)
       
-      let reward = null
       if (randomChance == 1) {
         const randomIndex = Math.floor(Math.random() * this.rewards.length)
-        reward = this.rewards[randomIndex]
+        const reward = this.rewards[randomIndex]
+        this.setAlertContent(reward.icon, reward.name, reward.comment, ['Hurray!']) 
         // Play animation and spread the good word
-        console.log(`Congrats! ${reward.comment}`)
       } else {
         console.log('No reward this time')
+        this.setAlertContent('', 'Nothing this time', 'Keep up the good work!', ['Oh well!'])
       }
+      
+      this.setOpen(true)
+    },
+    setAlertContent(
+      header: string, 
+      subHeader: string, 
+      message: string, 
+      buttons: string[]
+    ) {
+      this.alertContent.header = header
+      this.alertContent.subHeader = subHeader
+      this.alertContent.message = message
+      this.alertContent.buttons = buttons
     }
   }
 });

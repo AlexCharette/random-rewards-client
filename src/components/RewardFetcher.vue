a73fb36c99caf197349e5627d1dda35280c8617f
Alexander Charette <ga_charette@pm.me>
2022-11-22 21:08:05 +0100

    Switch reward attributes to uppercase to match schema

--- src/components/RewardFetcher.vue
+++ src/components/RewardFetcher.vue
@@ -41,15 +41,13 @@ export default defineComponent({
   },
   methods: {
     fetchReward() {
-      const randomChance = Math.floor(Math.random() * 3)
-      
+      const randomChance = Math.floor(Math.random() * 3)  
       if (randomChance == 1) {
         const randomIndex = Math.floor(Math.random() * this.rewards.length)
         const reward = this.rewards[randomIndex]
-        this.setAlertContent(reward.icon, reward.name, reward.comment, ['Hurray!']) 
+        this.setAlertContent(reward.Icon, reward.Name, reward.Comment, ['Hurray!']) 
         // Play animation and spread the good word
       } else {
-        console.log('No reward this time')
         this.setAlertContent('', 'Nothing this time', 'Keep up the good work!', ['Oh well!'])
       }
       
